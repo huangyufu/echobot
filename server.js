6c07507c1c31f9e70bb94fe8fc8d23584696d0b9
huangyufu <huangyufu@gmail.com>
2016-12-15 13:32:48 -0600

    10 Commit

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: 66fad73c-acac-4cdd-b3ea-247ed97fea6f,
+    appPassword: zJovkf6aWC4aBhWyKyL7MYi
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
